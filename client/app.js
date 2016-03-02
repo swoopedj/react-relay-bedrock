f154871841634488e7c854f46d8b89c2b418beda
Clay Branch <clay.branch@gmail.com>
2016-03-02 12:21:33 -0600

    basic react component loading

--- client/app.js
+++ client/app.js
@@ -1,2 +1,15 @@
-var func = () => 5;
-func(5);
+var React = require('react');
+var ReactDOM = require('react-dom');
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('root')
+);
