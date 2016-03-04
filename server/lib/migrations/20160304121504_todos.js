21bb3e2cd1d88df09a437615512f3b32d0721b56
Clay Branch <clay.branch@gmail.com>
2016-03-04 14:20:21 -0600

    updated migrations

--- server/lib/migrations/20160304121504_todos.js
+++ server/lib/migrations/20160304121504_todos.js
@@ -1,18 +1,6 @@
 exports.up = (knex, Promise) => {
   return Promise.all([
-    knex.schema.createTable('lists', (table) => {
-      table.increments(); // integer id
-
-      table.timestamp('created_at').defaultTo(knex.raw('now()')).notNullable();
-
-      // It's null if the list is public
-      table.integer('user_id').nullable();
-
-      // The name will be the same as the ID
-      table.string('name').defaultTo(knex.raw('"List "||currval("lists_id_seq")')).notNullable();
-    }),
-
-    knex.schema.createTable('todos', (table) => {
+    knex.schema.createTableIfNotExists('todos', (table) => {
       table.increments(); // integer id
 
       table.timestamp('created_at').defaultTo(knex.raw('now()')).notNullable();
