b274ea1ea131bfd710166ac5111396a2afe8c255
Clay Branch <clay.branch@gmail.com>
2016-03-05 11:50:57 -0600

    fixed linter errors with use strict

--- test/server/apis/todos-api_test.js
+++ test/server/apis/todos-api_test.js
@@ -1,35 +1,28 @@
-require(TEST_HELPER)
-const db = require(__server + '/lib/db');
+/* global TEST_HELPER describe it_ TestHelper __server beforeEach expect */
+'use strict';
+require(TEST_HELPER);
 const sinon = require('sinon');
-const Todo = require(__server + '/models/todos');
-var request = require('supertest');
+const Todo = require(`${__server}/models/todos`);
+const request = require('supertest');
 require('sinon-as-promised');
 
-var app = null;
-describe("The Todos API", function() {
-  beforeEach(function(){
-  	app = TestHelper.createApp();
-
+describe('The Todos API', () => {
+  let app = null;
+  beforeEach(() => {
+    app = TestHelper.createApp();
   });
 
-  describe('GET /todos', function(){
-    it_.only('returns all todos', function * (){
-        var read = sinon.stub(Todo, 'read')
-        read.resolves([{text : "testing"}]);
-        yield request(app)
-		      .get('/api/todos')
-		      .expect(200)
-		      .expect(function(response) {
-		        expect(response.body.todos).to.include({text : "testing"});
-		      })
-		    read.restore();
+  describe('GET /todos', () => {
+    it_('returns all todos', function * testTodos() {
+      const read = sinon.stub(Todo, 'read');
+      read.resolves([{ text: 'testing' }]);
+      yield request(app)
+				.get('/api/todos')
+				.expect(200)
+				.expect(response => {
+  expect(response.body.todos).to.include({ text: 'testing' });
+				});
+      read.restore();
     });
-
   });
-  
-
-
 });
-
-
-
